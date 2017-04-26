bd0dc5d56855d626f09bf02dac50e81c1adf971e
kanit sharma <kanitshrm09@gmail.com>
2017-04-27 02:04:20 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MdButtonModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +16,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MdButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
